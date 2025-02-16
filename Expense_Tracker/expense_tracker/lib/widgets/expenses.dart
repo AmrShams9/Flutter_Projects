c78d0ab2775a571d69f09545cf5304a7cb068424
Amr Shams <amrkhaled104@gmail.com>
2025-02-16 21:32:32 +0200

    huig

--- Expense_Tracker/expense_tracker/lib/widgets/expenses.dart
+++ Expense_Tracker/expense_tracker/lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
@@ -27,10 +28,27 @@ class _ExpensesState extends State<Expenses> {
       category: Category.leisure,
     ),
   ];
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Expense Tracker"),
+        actions: [
+          IconButton(
+              onPressed: _openAddExpenseOverlay,
+              icon: Icon(
+                Icons.add,
+                color: Colors.black,
+              )),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The chart'),
---
+++ Expense_Tracker/expense_tracker/lib/widgets/new_expense.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = '';
+  var _titleController = TextEditingController();
+  var _amountController = TextEditingController();
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(label: Text("Title")),
+          ),
+          Column(
+            children: [
+              TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    prefixText: '\$', label: Text("Amount")),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel")),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: Text("Save Expense"))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
